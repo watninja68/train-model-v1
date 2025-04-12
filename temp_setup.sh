cc0f23066a4aef291821f6ef50aa05264edf1d10
karneeshkar <karneeshkar68@gmail.com>
2025-04-12 09:39:37 +0530

    minor

--- temp_setup.sh
+++ temp_setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-#./run.sh
+./run.sh python
+uv venv
+source .venv/bin/activate
 uv pip   install torch torchvision torchaudio
-pip install --no-deps git+https://github.com/huggingface/transformers@v4.49.0-Gemma-3
 uv pip   install -r requirement.txt
+uv pip install --no-deps git+https://github.com/huggingface/transformers@v4.49.0-Gemma-3
 uv run "llama3_1_(8b)_grpo.py"
