2826ac0f4c035353b9e2c07b7338642ea033f230
karneeshkar <karneeshkar68@gmail.com>
2025-04-12 08:47:08 +0530

    requirement changes

--- temp_setup.sh
+++ temp_setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #./run.sh
 uv pip   install torch torchvision torchaudio
+pip install --no-deps git+https://github.com/huggingface/transformers@v4.49.0-Gemma-3
 uv pip   install -r requirement.txt
 uv run "llama3_1_(8b)_grpo.py"
