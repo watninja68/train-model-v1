c624eee4b5d2aaa07e97cf151283f1135fe4219e
karneeshkar <karneeshkar68@gmail.com>
2025-04-14 08:07:53 +0530

    should_be_final

--- temp_setup.sh
+++ temp_setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 ./run.sh python
+ollama pull gemma3:27b
 uv venv
 source .venv/bin/activate
+uv pip install re typing time json requests 
 uv pip   install torch torchvision torchaudio
 uv pip   install -r requirement.txt
 uv pip install --no-deps git+https://github.com/huggingface/transformers@v4.49.0-Gemma-3
